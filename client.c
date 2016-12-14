659b6ca63c52e040c8fafbea145ea1cf455a2112
AlexTryjan <aet41@case.edu>
2016-12-13 20:52:08 -0500

    exit properly terminates client
    Signed-off-by: AlexTryjan <aet41@case.edu>

--- client.c
+++ client.c
@@ -6,7 +6,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <pthread.h>
 
+
+//GLOBAL VARIABLES
+int sockfd;
+int is_terminating = 0;
+char exit_string[256] = "/exit";
+
+//METHODS
+void *write_to_server();
+void *read_from_server();
+
+//OTHER VARIABLES
 void error(const char *msg)
 {
     perror(msg);
@@ -29,12 +41,13 @@ int compare(char str1[], char str2[])
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n, pid;
+    int portno, n, pid;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    pthread_t tid1; /* the thread identifiers */
+    pthread_t tid2;
 
     char buffer[256];
-    char exit_string[256] = "/exit";
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -66,29 +79,50 @@ int main(int argc, char *argv[])
 
     bzero(buffer,256);
 
-    pid = fork();
-    while(pid)
+    pthread_create(&tid1, NULL, write_to_server, NULL);
+    pthread_create(&tid2, NULL, read_from_server, NULL);
+    
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    return 0;
+}
+
+void *write_to_server()
+{
+    char buffer[256];
+    while(1)
     {
         sleep(1);
         printf("> ");
         bzero(buffer,256);
         fgets(buffer,255,stdin);
-        if(compare(buffer,exit_string))
+        if(buffer[0] == '\n')
         {
-            close(sockfd);
-            return 0;
+            continue;
         }
-        n = write(sockfd,buffer,strlen(buffer));
-        if (n < 0)
-            error("ERROR writing to socket");
+        else if(compare(buffer,exit_string))
+        {
+            is_terminating = 1;
+            close(sockfd);
+            pthread_exit(0);
+        }
+        write(sockfd,buffer,strlen(buffer));
     }
-    while(!pid) {
+}
+
+void *read_from_server()
+{
+    char buffer[256];
+    while(1)
+    {
         fflush(stdout);
-        n = read(sockfd,buffer,255);
-        if (n < 0)
-            error("ERROR reading from socket");
+        read(sockfd,buffer,255);
         if(strlen(buffer) != 0)
-          printf("%s",buffer);
+            printf("%s",buffer);
         bzero(buffer,256);
+        if(is_terminating)
+        {
+            pthread_exit(0);
+        }
     }
 }
